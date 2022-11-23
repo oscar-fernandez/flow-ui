fa3d0185c8afdfce7473bfc1e821ae9d463c56ab
Oscar.Fernandez <959357@cognizant.com>
2022-11-23 10:57:13 -0500

    Refactor App.test, re-commit

--- src/pages/App/App.test.tsx
+++ src/pages/App/App.test.tsx
@@ -1,13 +1,10 @@
 import { describe, it, expect } from "vitest";
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-describe("Dummy tests", () => {
-    it("should be true", () => {
-        expect(true).toBe(true);
-    })
-    it("should render App", () => {
-        render(<App />);
-        expect(screen.getByText("Flow-E")).toBeInTheDocument();
-    })
-})
+describe("App", () => {
+  it("should render PageContainer", () => {
+    render(<App />);
+    expect(screen.getByText("Menu-side-bar")).toBeInTheDocument();
+  });
+});
