c8acecdf15ca487c9fa13a640fac77ca0c444792
Wikel <riley.wikel@cognizant.com>
2022-11-28 16:44:46 -0600

    Finished up styling for the page number carousel component, now implementing logic to page numbers.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App/App";
+import PageNumberCarousel from "./components/PageNumberCarousel/PageNumberCarousel";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <PageNumberCarousel />
   </React.StrictMode>
 );
