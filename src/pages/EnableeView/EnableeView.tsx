ce3c780c61083a0918f300696177f44345cbe49f
Wikel <riley.wikel@cognizant.com>
2022-12-01 16:32:14 -0600

    Added components and necessary functionality to enablee view

--- src/pages/EnableeView/EnableeView.tsx
+++ src/pages/EnableeView/EnableeView.tsx
@@ -6,30 +6,35 @@ import Row from "../../components/RowComponent/Row";
 import { dummyEnablees } from "./EnableeMock";
 
 export function EnableeView() {
-  const [toggle, setToggle] = useState(false);
-  const [index, setIndex] = useState(1);
+  const [ontoggle, setToggle] = useState(false);
 
   return (
     <>
+      <span className="header-section">
+        <h1 className="header">Enablees</h1>
+        <button className="header-btn">+</button>
+      </span>
+
       <Filter
         inputOne="employee id"
         inputTwo="first name"
         inputThree="last name"
         inputFour="tech stack"
       />
-      <p>hi</p>
+
       {dummyEnablees.map((enablee, i) => {
-        <Row
-          id={enablee.employeeId}
-          firstName={enablee.firstName}
-          lastName={enablee.lastName}
-          techStack={["Java", "React", "Rust", "C++"]}
-          key={i}
-          onClick={() => {
-            setToggle(!ontoggle);
-            setIndex(i);
-          }}
-        />;
+        return (
+          <Row
+            key={i}
+            id={enablee.employeeId}
+            firstName={enablee.firstName}
+            lastName={enablee.lastName}
+            techStack={["Java", "React", "Rust", "C++"]}
+            onClick={() => {
+              setToggle(!ontoggle);
+            }}
+          />
+        );
       })}
       <PageNumberCarousel totalPages={10} />
     </>
