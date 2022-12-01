5b3c6cdd9228db4087c451a7a02575bff94a2e35
lilianne.tavarez <lilianne.tavarez>
2022-12-01 16:58:08 -0500

    enableeView

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App/App";
+import { EnableeView } from "./pages/EnableeView/EnableeView";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <EnableeView />
   </React.StrictMode>
 );
--- src/pages/EnableeView/EnableeView.tsx
+++ src/pages/EnableeView/EnableeView.tsx
@@ -1,7 +1,14 @@
 import { Filter } from "../../components/Filter/Filter";
 import "./EnableeView.css";
+import { useState } from "react";
+import PageNumberCarousel from "../../components/PageNumberCarousel/PageNumberCarousel";
+import Row from "../../components/RowComponent/Row";
+import { dummyEnablees } from "./EnableeMock";
 
-function EnableeView() {
+export function EnableeView() {
+  const [toggle, setToggle] = useState(false);
+  const [index, setIndex] = useState(1);
+
   return (
     <>
       <Filter
@@ -10,8 +17,21 @@ function EnableeView() {
         inputThree="last name"
         inputFour="tech stack"
       />
+      <p>hi</p>
+      {dummyEnablees.map((enablee, i) => {
+        <Row
+          id={enablee.employeeId}
+          firstName={enablee.firstName}
+          lastName={enablee.lastName}
+          techStack={["Java", "React", "Rust", "C++"]}
+          key={i}
+          onClick={() => {
+            setToggle(!ontoggle);
+            setIndex(i);
+          }}
+        />;
+      })}
+      <PageNumberCarousel totalPages={10} />
     </>
   );
 }
-
-export default EnableeView;
