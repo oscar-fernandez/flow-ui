62bfad2bc66f4e550cf8854bfa418cdc8d7d3b49
Sweetman <955361@cognizant.com>
2022-12-02 17:13:51 -0500

    ToggleSidebar global context made and live tested with dummy rows and enablees.

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,10 +1,13 @@
+import ToggleProvider from "../../context/ToggleSideBarContext";
 import PageContainer from "../PageContainer/PageContainer";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <PageContainer />
+      <ToggleProvider>
+        <PageContainer />
+      </ToggleProvider>
     </>
   );
 }
