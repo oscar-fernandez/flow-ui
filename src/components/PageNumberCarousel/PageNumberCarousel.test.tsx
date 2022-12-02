46f1fe44469c8416d961a6c034bcc24e74aeb37b
Sweetman <955361@cognizant.com>
2022-12-02 11:09:34 -0500

    Rebase

--- src/components/PageNumberCarousel/PageNumberCarousel.test.tsx
+++ src/components/PageNumberCarousel/PageNumberCarousel.test.tsx
@@ -1,103 +1,97 @@
-import { beforeEach, describe, it, expect, vi } from "vitest";
+import { beforeAll, describe, it } from "vitest";
 import { render, screen, fireEvent } from "@testing-library/react";
 import PageNumberCarousel from "./PageNumberCarousel";
 
-describe("PageNumberCarousel", () => {
-  beforeEach(() => {
-    render(<PageNumberCarousel totalPages={10} />);
+describe("PageNumber with one page", () => {
+  beforeAll(() => {
+    render(<PageNumberCarousel totalPages={1} />);
   });
+  it("'Previous page' button is disabled"),
+    () => {
+      const previousPageButton = screen.getByRole("button", {
+        name: "Previous page",
+      });
+      expect(previousPageButton).toBeDisabled();
+    };
+  it("'Next page' button is disabled'"),
+    () => {
+      const nextPageButton = screen.getByRole("button", {
+        name: "Next page",
+      });
+      expect(nextPageButton).toBeDisabled();
+    };
+  it("'Go button' is disabled"),
+    () => {
+      const goButton = screen.getByRole("button", { name: "Go" });
+      expect(goButton).toBeDisabled();
+    };
+  it("Link to page 1 is current page"),
+    () => {
+      const pageOneLink = screen.getByRole("link", { name: "1" });
+      expect(pageOneLink).toHaveStyle("color: #DC8D0B");
+    };
+});
 
-  it('should have a "Next page" button', () => {
-    const nextPageButton = screen.getByRole("button", {
-      name: "Next page",
-    });
-    expect(nextPageButton).toBeTruthy();
+describe("PageNumber with multiple pages", () => {
+  beforeAll(() => {
+    render(<PageNumberCarousel totalPages={5} />);
   });
-
-  it('should enable previous page button after clicking "Next page"', () => {
-    const previousPageButton = screen.getByRole("button", {
-      name: "Previous page",
-    });
-    const nextPageButton = screen.getByRole("button", {
-      name: "Next page",
-    });
-    fireEvent.click(nextPageButton);
-    expect(previousPageButton).toBeEnabled();
-  });
-
-  it('should disable "Next Page" when on last page', () => {
-    const nextPageButton = screen.getByRole("button", {
-      name: "Next page",
-    });
-    for (let i = 0; i < 9; i++) {
+  it("'Next page' button is enabled"),
+    () => {
+      const nextPageButton = screen.getByRole("button", {
+        description: "Next page",
+      });
+      expect(nextPageButton).toBeEnabled();
+    };
+  it("Last page number link is equal to totalPages prop"),
+    () => {
+      expect(screen.getByRole("link", { name: "5" })).toBeInTheDocument();
+    };
+  it("'...' is present when totalPages is greater than 3"),
+    () => {
+      expect(screen.getByText("...")).toBeInTheDocument();
+    };
+  it("Link to page 2 is current after clicking next page"),
+    () => {
+      const nextPageButton = screen.getByRole("button", {
+        description: "Next page",
+      });
+      const pageTwoLink = screen.getByRole("link", { name: "5" });
       fireEvent.click(nextPageButton);
-    }
-    expect(nextPageButton).toBeDisabled();
-  });
-
-  it('should disable "Previous page" button when on first page', () => {
-    const nextPageButton = screen.getByRole("button", {
-      name: "Next page",
-    });
-    const previousPageButton = screen.getByRole("button", {
-      name: "Previous page",
-    });
-    fireEvent.click(nextPageButton);
-    fireEvent.click(previousPageButton);
-    expect(previousPageButton).toBeDisabled();
-  });
-
-  it("should have both buttons enabled", () => {
-    const nextPageButton = screen.getByRole("button", {
-      name: "Next page",
-    });
-    const previousPageButton = screen.getByRole("button", {
-      name: "Previous page",
-    });
-    for (let i = 0; i < 4; i++) {
+      expect(pageTwoLink).toHaveStyle("color: #DC8D0B");
+    };
+  it(
+    "Link to page 1 is current after clicking on next page and then previous page"
+  ),
+    () => {
+      const nextPageButton = screen.getByRole("button", {
+        description: "Next page",
+      });
+      const previousPageButton = screen.getByRole("button", {
+        name: "Previous page",
+      });
+      const pageOneLink = screen.getByRole("link", { name: "1" });
       fireEvent.click(nextPageButton);
-    }
-    fireEvent.click(previousPageButton);
-    expect(previousPageButton).toBeEnabled();
-    expect(nextPageButton).toBeEnabled();
-  });
-
-  it("goes to page that has been clicked", () => {
-    fireEvent.click(screen.getByText("4"));
-    expect(screen.getByText("4")).toHaveClass("number", "active");
-    fireEvent.click(screen.getByText("1"));
-    expect(screen.getByText("1")).toHaveClass("number", "active");
-    fireEvent.click(screen.getByText("10"));
-    expect(screen.getByText("10")).toHaveClass("number", "active");
-  });
-
-  it("goes to the page that is submitted by the user", () => {
-    const input = screen.getByRole("textbox");
-    const goButton = screen.getByRole("button", { name: "Go" });
-    fireEvent.change(input, { target: { value: "5" } });
-    fireEvent.click(goButton);
-    expect(screen.getByText("5")).toHaveClass("number", "active");
-    fireEvent.change(input, { target: { value: "100" } });
-    fireEvent.click(goButton);
-    expect(screen.getByText("5")).toHaveClass("number", "active");
-  });
-
-  it("should enable/disable previous and next page buttons correctly", () => {
-    const nextPageButton = screen.getByRole("button", {
-      name: "Next page",
-    });
-    const previousPageButton = screen.getByRole("button", {
-      name: "Previous page",
-    });
-    fireEvent.click(screen.getByText("10"));
-    expect(previousPageButton).toBeEnabled();
-    expect(nextPageButton).toBeDisabled();
-    fireEvent.click(screen.getByText("7"));
-    expect(previousPageButton).toBeEnabled();
-    expect(nextPageButton).toBeEnabled();
-    fireEvent.click(screen.getByText("10"));
-    fireEvent.click(screen.getByText("1"));
-    expect(nextPageButton).toBeEnabled();
-    expect(previousPageButton).toBeDisabled();
-  });
+      fireEvent.click(previousPageButton);
+      expect(pageOneLink).toHaveStyle("color: #DC8D0B");
+    };
+  it("Current page corresponds to user input"),
+    () => {
+      const pageInput = screen.getByRole("input");
+      const goButton = screen.getByRole("button", { name: "Go" });
+      const pageFourLink = screen.getByRole("link", { name: "4" });
+      expect(pageFourLink).toHaveStyle("color: #000048");
+      fireEvent.change(pageInput, { target: { value: "4" } });
+      fireEvent.click(goButton);
+      expect(pageFourLink).toHaveStyle("color: #DC8D0B");
+    };
+  it("'Next page' button is disabled while on last page"),
+    () => {
+      const pageFiveLink = screen.getByRole("link", { name: "5" });
+      const nextPageButton = screen.getByRole("button", {
+        name: "Next page",
+      });
+      fireEvent.click(pageFiveLink);
+      expect(nextPageButton).toBeDisabled();
+    };
 });
