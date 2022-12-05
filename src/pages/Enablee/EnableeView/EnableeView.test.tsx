f6d8343c7c382efcb7d89bd4ef7f75ef863dc554
Wikel <riley.wikel@cognizant.com>
2022-12-05 15:29:07 -0600

    Made some orginaztional adjustments to the project

---
+++ src/pages/Enablee/EnableeView/EnableeView.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { describe, it } from "vitest";
+import { EnableeView } from "./EnableeView";
+
+describe("Enablee View page", () => {
+  it("should display enablee view page with correct components", () => {
+    render(<EnableeView />);
+  });
+});
---
+++ src/pages/Enablee/EnableeView/EnableeView.tsx
@@ -0,0 +1,25 @@
+import { Filter } from "../../../components/Filter/Filter";
+import "./EnableeView.css";
+import PageNumberCarousel from "../../../components/PageNumberCarousel/PageNumberCarousel";
+import { PageViewHeader } from "../../../components/HeaderSectionComponents/PageViewHeader/PageViewHeader";
+import { LoginComponent } from "../../../components/HeaderSectionComponents/LoginComponent/LoginComponent";
+import { GenerateRows } from "../../../components/GenerateRows/GenerateRows";
+
+export function EnableeView() {
+  return (
+    <>
+      <LoginComponent name="Ondrew" />
+      <div className="page-section">
+        <PageViewHeader pageTitle="Enablees" showPlus={true} />
+        <Filter
+          inputOne="employee id"
+          inputTwo="first name"
+          inputThree="last name"
+          inputFour="tech stack"
+        />
+        <GenerateRows />
+        <PageNumberCarousel totalPages={10} />
+      </div>
+    </>
+  );
+}
