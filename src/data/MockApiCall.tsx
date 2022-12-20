e12c75cd335d492485e0945849ce1e00fa51441b
Kiecun <2211117@cognizant.com>
2022-12-20 12:59:20 -0500

    NeedsMoreLinesTested

---
+++ src/data/MockApiCall.tsx
@@ -0,0 +1,64 @@
+import IProject from "../models/interfaces/IProject";
+
+export const dummyProjects: IProject[] = [
+  {
+    id: 977284,
+    name: "Steve",
+    summary: "Bob",
+    technology: [
+      { id: 2, name: "Java" },
+      { id: 8, name: "React" },
+      { id: 12, name: "Rust" },
+      { id: 12, name: "C++" },
+    ],
+    repoLink: "google.com",
+  },
+  {
+    id: 977284,
+    name: "Steve",
+    summary: "Bob",
+    technology: [
+      { id: 2, name: "Java" },
+      { id: 8, name: "React" },
+      { id: 12, name: "Rust" },
+      { id: 12, name: "C++" },
+    ],
+    repoLink: "google.com",
+  },
+  {
+    id: 977284,
+    name: "Steve",
+    summary: "Bob",
+    technology: [
+      { id: 2, name: "Java" },
+      { id: 8, name: "React" },
+      { id: 12, name: "Rust" },
+      { id: 12, name: "C++" },
+    ],
+    repoLink: "google.com",
+  },
+  {
+    id: 977284,
+    name: "Steve",
+    summary: "Bob",
+    technology: [
+      { id: 2, name: "Java" },
+      { id: 8, name: "React" },
+      { id: 12, name: "Rust" },
+      { id: 12, name: "C++" },
+    ],
+    repoLink: "google.com",
+  },
+  {
+    id: 977284,
+    name: "mark",
+    summary: "steve",
+    technology: [
+      { id: 2, name: "Java" },
+      { id: 8, name: "React" },
+      { id: 12, name: "Rust" },
+      { id: 12, name: "C++" },
+    ],
+    repoLink: "google.com",
+  },
+];
--- src/pages/Management/ManagementTabsComponent/ManagementTabs.tsx
+++ src/pages/Management/ManagementTabsComponent/ManagementTabs.tsx
@@ -5,9 +5,16 @@ import Typography from "@mui/material/Typography";
 import FormComponent from "../../../components/FormComponent/FormComponent";
 import Box from "@mui/material/Box";
 import ManagementTableComponent from "../../../components/Table/ManagementTableComponent/ManagementTableComponent";
-import { useRef, useState } from "react";
-import { MockRows, MockData } from "../../../data/MockData";
+import { useRef, useState, useEffect } from "react";
+import { MockRows } from "../../../data/MockData";
 import IColumns from "../../../models/interfaces/IColumns";
+import { getProjects, getTechnologies } from "../../../services/ManagementAPI";
+import {
+  updatedTechnology,
+  updatedProjects,
+} from "../../../utils/utilityFunctions";
+import IProjectTable from "../../../models/interfaces/IProjectTable";
+import ITechnologyTable from "../../../models/interfaces/ITechnologyTable";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -34,8 +41,6 @@ export const tabItemStyles = () => ({
 
 function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
-  const projectColumn: IColumns = { topics: ["projectName", "techStack"] };
-  const technologyColumn: IColumns = { topics: ["skillName"] };
 
   return (
     <div
@@ -65,6 +70,32 @@ export default function ManagementTabs() {
   const [value, setValue] = React.useState(0);
   const selectedItem = useRef("");
   const [useBoolean, setBoolean] = useState(false);
+  const projectColumn: IColumns = { topics: ["projectName", "techStack"] };
+  const technologyColumn: IColumns = { topics: ["skillName"] };
+  let projectRows: IProjectTable[] = [];
+  let technologyRows: ITechnologyTable[] = [];
+
+  //making api call here
+  const getAll = async () => {
+    getTechnologies()
+      .then((res) => {
+        technologyRows = updatedTechnology(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    getProjects()
+      .then((res) => {
+        projectRows = updatedProjects(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    getAll();
+  }, []);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -98,12 +129,22 @@ export default function ManagementTabs() {
           </Tabs>
         </Box>
         <TabPanel value={value} index={0}>
-          <FormComponent title="Add Project" readonly={false} />
+          {useBoolean ? (
+            <FormComponent title="Add Project" readonly={false} />
+          ) : (
+            <ManagementTableComponent
+              selectedItem={selectedItem}
+              columns={projectColumn}
+              rows={MockRows}
+              selectedBoolean={setBoolean}
+              currentBoolean={useBoolean}
+            />
+          )}
         </TabPanel>
         <TabPanel value={value} index={1}>
           <ManagementTableComponent
             selectedItem={selectedItem}
-            columns={MockData}
+            columns={technologyColumn}
             rows={MockRows}
             selectedBoolean={setBoolean}
             currentBoolean={useBoolean}
