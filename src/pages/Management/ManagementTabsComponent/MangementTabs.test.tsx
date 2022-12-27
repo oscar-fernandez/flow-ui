fbdb47c34c60286e9ef18b5b8846af36dcfc293e
Merry.Degaga <merry.degaga@cognizant.com>
2022-12-27 08:43:51 -0500

    mgt tab test

--- src/pages/Management/ManagementTabsComponent/MangementTabs.test.tsx
+++ src/pages/Management/ManagementTabsComponent/MangementTabs.test.tsx
@@ -1,6 +1,7 @@
 import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import ManagementTabs from "./ManagementTabs";
+import { SyntheticEvent } from "react";
 
 vi.mock("react-router-dom", () => ({
   useNavigate: () => vi.fn(),
@@ -8,7 +9,16 @@ vi.mock("react-router-dom", () => ({
 
 describe("ManagementTabs", () => {
   it("should render ManagementTabs", () => {
-    render(<ManagementTabs />);
+    render(
+      <ManagementTabs
+        handleChange={function (
+          e: SyntheticEvent<Element, Event>,
+          newValue: number
+        ): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
     expect(screen.getByText("Projects")).toBeInTheDocument();
   });
 });
