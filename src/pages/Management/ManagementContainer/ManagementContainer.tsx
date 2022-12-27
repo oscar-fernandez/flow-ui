abdc067a13734dd8ac457aad31bf5d690a7ae499
noahtorres98 <noahstorres98@gmail.com>
2022-12-27 14:01:31 -0600

    added header switch case and styled add button

--- src/pages/Management/ManagementContainer/ManagementContainer.tsx
+++ src/pages/Management/ManagementContainer/ManagementContainer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { PageViewHeader } from "../../../components/HeaderSectionComponents/PageViewHeader/PageViewHeader";
 import CustomTableContainer from "../../../components/Table/CustomTableContainer";
 import ManagementTabs from "../ManagementTabsComponent/ManagementTabs";
@@ -6,6 +6,44 @@ import FormComponent from "../../../components/FormComponent/FormComponent";
 import { dummyProjects as mockProjects } from "../../../data/MockApiCall";
 import { mockTechnology } from "../../../data/MockData";
 import * as Module from "../mgtUtils";
+import { Box, Button } from "@mui/material";
+
+const headerStyle = {
+  minWidth: 50,
+  background: "#E6E8E6",
+  fontWeight: 700,
+  fontSize: "24px",
+  color: "#000048",
+  borderRight: "1px solid #000048",
+  "&:last-child": { borderRight: "none" },
+};
+
+const cellStyle = {
+  fontSize: "18px",
+  border: "none",
+  color: "inherit",
+};
+
+const rowStyle = {
+  border: "5px solid black",
+  "&.MuiTableRow-root:hover": {
+    cursor: "pointer",
+    backgroundColor: "#DC8D0B",
+    color: "#000048",
+  },
+};
+
+const buttonStyle = {
+  background: "#E6E8E6",
+  fontWeight: 900,
+  fontSize: "18px",
+  color: "#000048",
+  padding: "16px",
+  "&:hover": {
+    backgroundColor: "#E6E8E6",
+    opacity: "1",
+  },
+};
 
 export default function ManagementContainer() {
   const [showForm, setShowForm] = useState(false);
@@ -38,6 +76,17 @@ export default function ManagementContainer() {
     }
   }
 
+  function headers(): string[] {
+    switch (value) {
+      case "Projects":
+        return ["project name", "tech stack"];
+      case "Technology":
+        return ["skill name"];
+      default:
+        return ["no tab matches value"];
+    }
+  }
+
   return (
     <>
       <div>
@@ -48,15 +97,23 @@ export default function ManagementContainer() {
           <FormComponent />
         ) : (
           <>
-            <div onClick={toggleShowForm}>
-              + Add {value === "Technology" ? "Skill" : value}
+            <div style={{ backgroundColor: "#E6E8E6" }}>
+              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button
+                  variant="text"
+                  onClick={toggleShowForm}
+                  sx={buttonStyle}
+                >
+                  + Add {value === "Technology" ? "Skill" : value}
+                </Button>
+              </Box>
             </div>
             <CustomTableContainer
-              headers={["header"]}
+              headers={headers()}
               rows={fn()}
-              headerStyle={null}
-              rowStyle={null}
-              cellStyle={null}
+              headerStyle={headerStyle}
+              rowStyle={rowStyle}
+              cellStyle={cellStyle}
               customHandleSelection={customHandleSelection}
             />
           </>
