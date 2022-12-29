1918abcf2267e5ed09c8f9130066f24257d35f27
noahtorres98 <noahstorres98@gmail.com>
2022-12-29 17:49:25 -0600

    added more tests

--- src/pages/Management/ManagementContainer/ManagementContainer.test.tsx
+++ src/pages/Management/ManagementContainer/ManagementContainer.test.tsx
@@ -14,4 +14,20 @@ describe("Management View page", () => {
     const button = utils.getByTestId("button") as HTMLButtonElement;
     fireEvent.click(button);
   });
+
+  it("should handle tabs click and render appropriate view", () => {
+    const utils = render(<ManagementContainer />);
+    const technologyTab = utils.getByTestId("techTab");
+    fireEvent.click(technologyTab);
+    const addSkill = utils.getByTestId("button") as HTMLButtonElement;
+    expect(addSkill.value).toBe("");
+  });
+
+  it("should handle row selection", () => {
+    const utils = render(<ManagementContainer />);
+    const row = utils.getByText("PixelGram");
+    fireEvent.click(row);
+    const title = utils.getByText("Project Details");
+    expect(title).toBeInTheDocument();
+  });
 });
