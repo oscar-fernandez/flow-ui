86b84fa6908d3236cf7e109de419a2d5df68bf84
noahtorres98 <noahstorres98@gmail.com>
2023-01-04 14:55:32 -0600

    added mgt facade pattern

--- src/services/FacadePattern.ts
+++ src/services/FacadePattern.ts
@@ -9,4 +9,26 @@ const getEnablees = async (url: string, params = {}) => {
   return await axios.get(`${url}?${queryString}`);
 };
 
-export { getEnablees };
+const mgtGet = async (url: string) => {
+  return await axios.get(`${url}`);
+};
+
+const mgtPost = async (url: string, params = {}) => {
+  const queryString = Object.entries(params)
+    .map((param) => {
+      return `${param[0]}=${param[1]}`;
+    })
+    .join("&");
+  return await axios.post(`${url}?${queryString}`);
+};
+
+const mgtPut = async (url: string, params = {}) => {
+  const queryString = Object.entries(params)
+    .map((param) => {
+      return `${param[0]}=${param[1]}`;
+    })
+    .join("&");
+  return await axios.put(`${url}?${queryString}`);
+};
+
+export { getEnablees, mgtGet, mgtPost, mgtPut };
