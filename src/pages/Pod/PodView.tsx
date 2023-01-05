0e8e2dc25481ae05593c10c253a369050a5adc8b
noahtorres98 <noahstorres98@gmail.com>
2023-01-05 13:24:01 -0600

    created podview and imported pageViewHeader

---
+++ src/pages/Pod/PodView.tsx
@@ -0,0 +1,11 @@
+import { PageViewHeader } from "../../components/HeaderSectionComponents/PageViewHeader/PageViewHeader";
+
+function PodView() {
+  return (
+    <>
+      <PageViewHeader pageTitle="Pod" showPlus={true}></PageViewHeader>
+    </>
+  );
+}
+
+export default PodView;
