9f9e00496c6b83383ca1cf4ec9edc030cd1b00b4
noahtorres98 <noahstorres98@gmail.com>
2023-01-05 14:35:14 -0600

    added tech stack colors and get random color function

--- src/pages/Management/mgtUtils.tsx
+++ src/pages/Management/mgtUtils.tsx
@@ -5,6 +5,39 @@ import { convertToStringArr } from "../../utils/utilityFunctions";
 
 const tabLabels = ["Projects", "Technology", "Grade", "Country", "Community"];
 
+const techStackColors = [
+  "#007C77",
+  "#4C1A57",
+  "#084B83",
+  "#885053",
+  "#94C9A9",
+  "#4E4D5C",
+  "#805E73",
+  "#243B4A",
+  "#AA1155",
+  "#355070",
+  "#E56B6F",
+  "#3066BE",
+  "#5A7D7C",
+  "#6D5A72",
+  "#464D77",
+  "#36827F",
+  "#F9DB6D",
+  "#E6AF2E",
+  "#1A936F",
+  "#88D498",
+  "#403F4C",
+  "#4F6367",
+  "#2D2D2A",
+  "#86CD82",
+  "#114B5F",
+];
+
+function getRandomColor(): string {
+  const num = Math.floor(Math.random() * (24 - 0 + 1) + 0);
+  return techStackColors[num];
+}
+
 function convertTechArToStr(ar: ITechnology[]): string {
   return convertToStringArr(ar).join(", ");
 }
