3842266a7209c65a80c5dd4e0670c8601c82b8ac
noahtorres98 <noahstorres98@gmail.com>
2023-01-06 12:09:02 -0600

    created table input component and added test

--- src/components/Table/CustomTableContainer.tsx
+++ src/components/Table/CustomTableContainer.tsx
@@ -5,8 +5,8 @@ import { SxProps, Theme } from "@mui/material";
 import TableBody from "@mui/material/TableBody";
 import CustomTableRow from "./CustomTableRow";
 import TableHeader from "./TableHeader";
-import TextField from "@mui/material/TextField";
-import React, { useState } from "react";
+import React from "react";
+import TableInput from "./TableInput";
 
 interface Props {
   headers: string[];
@@ -37,40 +37,17 @@ const CustomTableContainer = ({
   skill,
   value,
 }: Props) => {
-  const [newTechValue, setNewTechValue] = useState("");
-
-  const handleNewTechEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const target = event.target as HTMLInputElement;
-    if (event.key === "Enter") {
-      setTechnology(target.value);
-      setSkill(!skill);
-      setNewTechValue("");
-    }
-  };
-
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
       <TableContainer data-testid="table-container" sx={{ maxHeight: 600 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHeader headers={headers} headerStyle={headerStyle} />
           {skill && value === "Technology" && (
-            <div style={{ marginLeft: "1rem" }}>
-              <TextField
-                id="newSkill"
-                type="text"
-                variant="standard"
-                autoComplete="off"
-                value={newTechValue}
-                error={newTechValue.trim().length === 0}
-                helperText={newTechValue === "" ? "* Invalid Skill Name" : " "}
-                onChange={(e) => setNewTechValue(e.target.value)}
-                onKeyDown={handleNewTechEnter}
-                inputProps={{
-                  "data-testid": "input",
-                  style: { padding: "14.5px 0px" },
-                }}
-              />
-            </div>
+            <TableInput
+              setTechnology={setTechnology}
+              setSkill={setSkill}
+              skill={skill}
+            ></TableInput>
           )}
           <TableBody>
             {rows.map((r: string[], index: number) => (
