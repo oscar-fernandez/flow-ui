cd245c1102bb0d33c142fca8b7fbc1fafaf86778
Wikel <riley.wikel@cognizant.com>
2023-01-09 14:44:18 -0600

    This component is dependent on other issues. Can't complete as of now.

---
+++ src/components/OnHoverMenuItems/OnHoverMenuItems.tsx
@@ -0,0 +1,8 @@
+import "./OnHoverMenuItems.css";
+
+export function OnHoverMenuItems(props: {
+  subMenuItems: Array<string> | undefined;
+  customClick: string | undefined;
+}) {
+  return <div className="hovermenuitems-container"></div>;
+}
