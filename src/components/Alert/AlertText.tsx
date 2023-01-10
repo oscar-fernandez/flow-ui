77337f2fe22294e20a39197403ed4b8b626d6769
noahtorres98 <noahstorres98@gmail.com>
2023-01-09 20:13:09 -0600

    updated onclick handler, added props interface.

--- src/components/Alert/AlertText.tsx
+++ src/components/Alert/AlertText.tsx
@@ -1,9 +1,13 @@
 import "./Alert.css";
 
-export default function AlertText(props: any) {
+interface Props {
+  text: string;
+}
+
+export default function AlertText({ text }: Props) {
   return (
     <>
-      <p className="text">{props.text}</p>
+      <p className="text">{text}</p>
     </>
   );
 }
