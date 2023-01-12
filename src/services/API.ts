17e69b0109304912f6d96b31fd8c19f547d1fbaf
noahtorres98 <noahstorres98@gmail.com>
2023-01-12 14:51:28 -0600

    updated axios get call to accept params

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,21 +1,11 @@
 import axios from "axios";
 
 const get = async (url: string, params = {}) => {
-  const queryString = Object.entries(params)
-    .map((param) => {
-      return `${param[0]}=${param[1]}`;
-    })
-    .join("&");
-  return await axios.get(`${url}?${queryString}`);
+  return await axios.get(url, { params });
 };
 
 const post = async (url: string, params = {}) => {
-  const queryString = Object.entries(params)
-    .map((param) => {
-      return `${param[0]}=${param[1]}`;
-    })
-    .join("&");
-  return await axios.post(`${url}?${queryString}`);
+  return await axios.post(url, { params });
 };
 
 const put = async (url: string, params = {}) => {
