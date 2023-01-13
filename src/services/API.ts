4718b545f7cdd8f1457ab1ff1ce8f7d5f0f57b2b
noahtorres98 <noahstorres98@gmail.com>
2023-01-13 15:01:07 -0600

    added any type for flexibility on post and put

--- src/services/API.ts
+++ src/services/API.ts
@@ -4,11 +4,11 @@ const get = (url: string, params = {}) => {
   return axios.get(url, { params });
 };
 
-const post = (url: string, params = {}) => {
+const post = (url: string, params: any) => {
   return axios.post(url, params);
 };
 
-const put = (url: string, params = {}) => {
+const put = (url: string, params: any) => {
   return axios.put(url, params);
 };
 
