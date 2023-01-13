75c685fb82b8a09e5c455f7e58d273e1dec5939d
noahtorres98 <noahstorres98@gmail.com>
2023-01-13 14:20:20 -0600

    debuggin for failing tests in mgt api. tests now passing

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const get = async (url: string, params = {}) => {
-  return await axios.get(url, { params });
+const get = (url: string, params = {}) => {
+  return axios.get(url, { params });
 };
 
-const post = async (url: string, params = {}) => {
-  return await axios.post(url, params);
+const post = (url: string, params = {}) => {
+  return axios.post(url, params);
 };
 
-const put = async (url: string, params = {}) => {
-  return await axios.post(url, params);
+const put = (url: string, params = {}) => {
+  return axios.put(url, params);
 };
 
 export { get, post, put };
