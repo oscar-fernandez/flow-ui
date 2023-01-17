125725e22bcae87ac7468bac9d564f4b77a8def7
Alazard-code <alazard57@gmail.com>
2023-01-17 11:08:00 -0600

    Created useToggle hook still need to write test for it,will start writing once implemented

--- src/components/SideBarItems/customHooks.test.tsx
+++
@@ -1,9 +0,0 @@
-import { vi, describe, it, expect } from "vitest";
-import { useState } from "react";
-import { useCustomNavigate, useToggle } from "./customHooks";
-import { renderHook } from "@testing-library/react";
-
-describe("SidebarItems Hook test", () => {
-  // it("useToggle should take in a boolean value and return that value plus the handleClick function",()=>{
-  // })
-});
