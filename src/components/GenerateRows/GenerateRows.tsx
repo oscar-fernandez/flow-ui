d1923cad19d982eb625d783bc730ad9b4aea4eb8
Sweetman <955361@cognizant.com>
2023-01-18 11:53:47 -0500

    Replaced mock data with the live enablee api data in generateRows/EnableeView

--- src/components/GenerateRows/GenerateRows.tsx
+++ src/components/GenerateRows/GenerateRows.tsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
-import { dummyEnablees } from "../../data/EnableeMock";
+import { useEffect, useState } from "react";
+import IEnablee from "../../models/interfaces/IEnablee";
+import { GetPaginatedEnablees } from "../../services/EnableeAPI";
 import Row from "../RowComponent/Row";
 
 export function GenerateRows() {
   const [ontoggle, setToggle] = useState(false);
+  const [enablees, setEnablees] = useState<IEnablee[]>([]);
+
+  useEffect(() => {
+    getEnablees(0);
+  }, []);
+
+  const getEnablees = async (pageNumber: number) => {
+    GetPaginatedEnablees(pageNumber)
+      .then((res) => {
+        setEnablees(res.data.items);
+      })
+      .catch((e) => console.error(e));
+  };
 
   return (
     <>
-      {dummyEnablees.map((enablee, i) => {
+      {enablees.map((enablee, i) => {
         return (
           <Row
             key={i}
