dca8e411bae03cf48e34e7c6c6d2d136c6a60b23
Oscar.Fernandez <959357@cognizant.com>
2023-01-18 12:24:28 -0500

    Update useAllEnablees hook with API call, add test

--- src/pages/Enablee/Hooks/useAllEnablees.ts
+++ src/pages/Enablee/Hooks/useAllEnablees.ts
@@ -6,16 +6,15 @@ import IPageOfItems from "../../../models/interfaces/IPageOfItems";
 export const useAllEnablees = () => {
   const [enablees, setEnablees] = useState<IPageOfItems<IEnablee> | null>(null);
 
-  const updateEnablees = () => {
-    //page number is hardcoded but will be updated to a variable
-    GetPaginatedEnablees(1).then((list) => {
-      setEnablees(list.data);
+  const updateEnablees = (pageNumber: number) => {
+    GetPaginatedEnablees(pageNumber).then((res) => {
+      setEnablees(res.data);
     });
   };
 
   useEffect(() => {
-    updateEnablees();
+    updateEnablees(1);
   }, []);
 
-  return [enablees, updateEnablees];
+  return { enablees, updateEnablees };
 };
