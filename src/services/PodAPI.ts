21731b1ba6fcda119ff25c37889f5db15889f242
Robert.Whitlock <bwhitlo96@gmail.com>
2023-01-30 11:32:40 -0600

    Git broke my other branch

--- src/services/PodAPI.ts
+++ src/services/PodAPI.ts
@@ -1,4 +1,5 @@
-import { get } from "./API";
+import { get, post, put } from "./API";
+import IFEPod from "../models/interfaces/IFEPod";
 
 const baseUrl = `${process.env.VITE_ENABLEMENT_FEMS}/pod`;
 
@@ -8,4 +9,24 @@ export const getPendingPods = () => {
 
 export const getCompletedPods = () => {
   return get(`${baseUrl}/completed`);
+};
+
+export const getPods = (pageNumber: Number) => {
+  return get(`${baseUrl}?page=${pageNumber}`);
+};
+
+export const getActivePods = () => {
+  return get(`${baseUrl}/active`);
+};
+
+export const getAvailablePods = () => {
+  return get(`${baseUrl}/available`);
+};
+
+export const createPod = (pod: IFEPod) => {
+  return post(`${baseUrl}/create`, pod);
+};
+
+export const updatePod = (pod: IFEPod) => {
+  return put(`${baseUrl}/update`, pod);
 };
