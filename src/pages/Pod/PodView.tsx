59b17ac4213286843b04a2df42c36330c1fc5805
noahtorres98 <noahstorres98@gmail.com>
2023-01-31 10:43:18 -0600

    added toggle sidebar

--- src/pages/Pod/PodView.tsx
+++ src/pages/Pod/PodView.tsx
@@ -1,11 +1,15 @@
 import { PageViewHeader } from "../../components/HeaderSectionComponents/PageViewHeader/PageViewHeader";
 import { Outlet } from "react-router-dom";
+import ToggleSidebar from "../../components/ToggleSideBar/ToggleSidebar";
+import EnableeTemplate from "../../components/EnableeTemplate/EnableeTemplate";
 
 function PodView() {
   return (
     <>
       <PageViewHeader pageTitle="Pod" showPlus={true}></PageViewHeader>
       <Outlet />
+      {/* need to change to pod template */}
+      <ToggleSidebar template={<EnableeTemplate></EnableeTemplate>} />
     </>
   );
 }
