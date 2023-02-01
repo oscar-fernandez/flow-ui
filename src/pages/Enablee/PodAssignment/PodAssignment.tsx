81e782706bf13ec71176fb9c5fe0ad91392cfd89
Oksana-Kaz <oksanakazjob@gmail.com>
2023-02-01 15:17:11 -0500

    updated podUtils.tsx and PodAssignment.tsx.

--- src/pages/Enablee/PodAssignment/PodAssignment.tsx
+++ src/pages/Enablee/PodAssignment/PodAssignment.tsx
@@ -143,7 +143,6 @@ export default function PodAssignment() {
         if (item.employeeId === enablee.employeeId) {
           remove = false;
         }
-        null;
       });
       return remove;
     });
