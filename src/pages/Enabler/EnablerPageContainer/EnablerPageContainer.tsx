120f09dcb69090ddd69921423025505504ed41f1
Wikel <riley.wikel@cognizant.com>
2023-02-15 16:15:06 -0600

    Almost done with enabler page container

--- src/pages/Enabler/EnablerPageContainer/EnablerPageContainer.tsx
+++ src/pages/Enabler/EnablerPageContainer/EnablerPageContainer.tsx
@@ -1,27 +1,26 @@
-import { Tooltip } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
-import PageNumberCarousel from "../../../components/PageNumberCarousel/PageNumberCarousel";
-import Row from "../../../components/RowComponent/Row";
-import { TagComponent } from "../../../components/TagComponent/Tag";
 import {
   useToggle,
   useToggleDetail,
 } from "../../../context/ToggleSideBarContext/ToggleSideBarContext";
-import IEnablee from "../../../models/interfaces/IEnablee";
-import ITechnology from "../../../models/interfaces/ITechnology";
+import IFEEnabler from "../../../models/interfaces/IFEEnabler";
 import {
   convertToStringArr,
   generateTags,
   tooltipString,
 } from "../../../utils/utilityFunctions";
+import Row from "../../../components/RowComponent/Row";
+import { Tooltip } from "@mui/material";
+import ITechnology from "../../../models/interfaces/ITechnology";
+import { TagComponent } from "../../../components/TagComponent/Tag";
 
 interface Props {
   hook: (location: string) => any[];
   displayPageCarousel: boolean;
 }
 
-export function EnableePageContainer({ hook, displayPageCarousel }: Props) {
+export function EnablerPageContainer({ hook, displayPageCarousel }: Props) {
   const location = useLocation();
   const [enablers, getEnablers] = hook(location.pathname);
   const [page, setPage] = useState(1);
@@ -32,8 +31,7 @@ export function EnableePageContainer({ hook, displayPageCarousel }: Props) {
     return Math.ceil(enablers.totalElements / 25);
   };
 
-  const getList = (): IEnablee[] => {
-    /* conditional checks if enablees is a PageOfItems<IEnablee> instead of IEnablee[] */
+  const getList = (): IFEEnabler[] => {
     if (enablers?.items) {
       return enablers.items;
     } else {
@@ -49,12 +47,8 @@ export function EnableePageContainer({ hook, displayPageCarousel }: Props) {
 
   return (
     <>
-      {/* conditional checks if enablees exists before mapping */}
       {getList()?.map((enabler, i) => {
         const tooltip = [...convertToStringArr(enabler.technology)];
-        const startDate = new Date(enabler.enablementStartDate);
-        const endDate = new Date(enabler.enablementEndDate);
-        const statusTag = generateTags(enabler);
         return (
           <Row
             key={i}
@@ -67,12 +61,12 @@ export function EnableePageContainer({ hook, displayPageCarousel }: Props) {
               <div className="square"></div>
             </div>
 
-            <div className="row-child row-name">
+            <div className="row-md-child row-name">
               <p className="row-primary">{`${enabler.firstName} ${enabler.lastName}`}</p>
               <p className="row-secondary">{enabler.employeeId}</p>
             </div>
 
-            <div style={{ margin: "0 25px" }}>
+            <div>
               <Tooltip
                 className="row-sm-child tags-container"
                 title={tooltipString(tooltip)}
@@ -94,35 +88,16 @@ export function EnableePageContainer({ hook, displayPageCarousel }: Props) {
             </div>
 
             <div className="row-lg-child date-container">
-              <p className="row-primary">Enablement Dates</p>
-              {enabler.enablementStartDate ? (
-                <p className="row-secondary">{`${startDate.toLocaleString(
-                  "en-US",
-                  { month: "long", day: "numeric", year: "numeric" }
-                )} - ${endDate.toLocaleString("en-us", {
-                  month: "long",
-                  day: "numeric",
-                  year: "numeric",
-                })}`}</p>
+              <p className="row-primary">Assigned Pods</p>
+              {enabler.numActivePods ? (
+                <p className="row-secondary">{enabler.numActivePods}</p>
               ) : (
                 <p className="row-secondary">Empty</p>
               )}
             </div>
-
-            <div className="row-lg-child">
-              <p className="row-secondary">Status</p>
-              <TagComponent name={statusTag.name} color={statusTag.color} />
-            </div>
           </Row>
         );
       })}
-      {displayPageCarousel ? (
-        <PageNumberCarousel
-          totalPages={enablers?.totalElements ? getTotalPages() : 0}
-          currentPageNumber={page}
-          setPage={setPage}
-        />
-      ) : null}
     </>
   );
 }
