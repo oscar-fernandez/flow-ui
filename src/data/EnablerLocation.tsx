d7a8c5f829191060b4a2fb9ef1e402140a834d6e
noahtorres98 <noahstorres98@gmail.com>
2023-02-16 10:26:47 -0600

    added enabler location data

---
+++ src/data/EnablerLocation.tsx
@@ -0,0 +1,26 @@
+export const city: string[] = [
+  "Chicago",
+  "Flushing",
+  "Philadelphia",
+  "Des Moines",
+  "Dayton",
+  "New Orleans",
+  "Charlotte",
+  "Newport Beach",
+  "Fort Worth",
+  "Spring",
+];
+
+export const state: string[] = [
+  "IL",
+  "MI",
+  "PA",
+  "IA",
+  "OH",
+  "LA",
+  "NC",
+  "CA",
+  "TX",
+];
+
+export const country: string[] = ["United States"];
