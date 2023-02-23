86b4c9c1983f2897bf60b0068113ed9d2b54ea26
piotr.tsishuk@cognizant.com <piotr.tsishuk@cognizant.com>
2023-02-23 16:51:48 -0500

    refactored EnaberView component to seed the Pods map and update the context

--- src/pages/Enabler/EnablerView/EnablerView.tsx
+++ src/pages/Enabler/EnablerView/EnablerView.tsx
@@ -1,12 +1,40 @@
 import "./EnablerView.css";
 import { PageViewHeader } from "../../../components/HeaderSectionComponents/PageViewHeader/PageViewHeader";
 import { Outlet } from "react-router";
+import { getActivePods, getPendingPods } from "../../../services/PodAPI";
+import IFEPod from "../../../models/interfaces/IFEPod";
+import { useEffect, useState } from "react";
+import { useMapDetail } from "../../../context/ToggleSideBarContext/ToggleSideBarContext";
 
 /**
  * This component is rendered as the parent route "/enabler"
  * @returns enabler view
  */
 export default function EnablerView() {
+  const [activePods, setActivePods] = useState<IFEPod[]>([]);
+  const [pendingPods, setPendingPods] = useState<IFEPod[]>([]);
+  const [map, setMap] = useMapDetail();
+
+  const seedPodMap = (): Map<string, IFEPod[]> => {
+    const podMap = new Map();
+
+    getActivePods().then((res) => {
+      setActivePods(res.data);
+    });
+
+    getPendingPods().then((res) => {
+      setPendingPods(res.data);
+    });
+
+    podMap.set("Active", activePods);
+    podMap.set("Pending", pendingPods);
+    return podMap;
+  };
+
+  useEffect(() => {
+    setMap(seedPodMap());
+  }, [activePods, pendingPods]);
+
   return (
     <>
       <div className="page-section">
